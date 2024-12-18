44caf269e452fcd3c2fc625ac2a6f01608286cac
I538984 <oliver.schirmer@sap.com>
2024-12-18 17:25:11 +0100

    add: demo flag

--- post_install.sh
+++ post_install.sh
@@ -12,7 +12,7 @@ cd /app
 echo "Installing GPU-dependent packages..."
 
 # Run the setup script for GPU-dependent packages
-/bin/bash ./setup.sh --diffoctreerast --mipgaussian
+/bin/bash ./setup.sh --diffoctreerast --mipgaussian --demo
 
 # Mark completion
 touch /app/.post_install_done
