3f4abfdcbcd7f3071f5ab7cc7740825e510188d8
I538984 <15632467+ncryptedV1@users.noreply.github.com>
2025-02-09 22:28:23 +0100

    remove: ninja version checking

--- post_install.sh
+++ post_install.sh
@@ -9,17 +9,9 @@ fi
 
 cd /app
 
-echo "Installing GPU-dependent base packages..."
-/bin/bash ./setup.sh --basic 
-
-echo "Testing ninja installation..."
-ninja --version
-echo $? # Exit code should be 0 if ninja is installed
-# if ninja is not installed, installation of flash-attn will take 2h instead of 3-5min
-
 echo "Installing GPU-dependent packages..."
 # Run the setup script for GPU-dependent packages
-/bin/bash ./setup.sh --xformers --flash-attn --vox2seq --spconv --kaolin --nvdiffrast --diffoctreerast --mipgaussian --demo
+/bin/bash ./setup.sh --basic --xformers --flash-attn --vox2seq --spconv --kaolin --nvdiffrast --diffoctreerast --mipgaussian --demo
 
 # Mark completion
 touch /app/.post_install_done
